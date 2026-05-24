75cae8ec64d7e89bafc38dfb21b95c8f5bd23b1b
Vitory393 <victoryakerele2@gmail.com>
2026-05-24 22:09:10 +0100

    feat: ui finished

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 import { Suspense } from "react";
 
+
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
@@ -18,7 +19,7 @@ export default function Home() {
             {!hasEnvVars ? (
               <EnvVarWarning />
             ) : (
-              <Suspense>
+              <Suspense fallback={<div>Loading...</div>}>
                 <AuthButton />
               </Suspense>
             )}
